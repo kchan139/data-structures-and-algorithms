86ace96c58e17b5d660fe992354755863d5a91a1
Khoa Tran <khoatrandd.139@gmail.com>
2023-09-30 15:55:27 +0700

    fix reverse

--- Special List/reverse.cpp
+++ Special List/reverse.cpp
@@ -4,7 +4,7 @@ void reverse(E* L, int n)
     Stack<E> * S = new AStack<E> ();
 
     for (int i = 0; i < n; i++)
-    S->push(L[i]);
+        S->push(L[i]);
 
     for (int i = 0; i < n; i++)
         L[i] = S->pop();
