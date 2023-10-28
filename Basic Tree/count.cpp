a5eaf510e7f497f1b3908c31b6e8f19cd1464f9c
Khoa Tran <khoatrandd.139@gmail.com>
2023-10-28 17:06:37 +0700

    add node count bin tree

---
+++ Basic Tree/count.cpp
@@ -0,0 +1,10 @@
+template <typename E>
+int count(BNode<E> * root)
+{
+    if (!root) return 0;
+ 
+    int left = count(root->left());
+    int right = count(root->right());
+ 
+    return 1 + left + right;
+}
