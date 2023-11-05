da76880b577d43c753eab95a979dbe0ce830153a
Khoa Tran <khoatrandd.139@gmail.com>
2023-11-05 11:40:20 +0700

    fixed 05

--- Advance Tree/largest_smaller.cpp
+++ Advance Tree/largest_smaller.cpp
@@ -1,39 +1,19 @@
 template <typename E>
 void LargestSmaller(BNode<E> *root, E key)
 {
-    vector<E> min;
-    helper(root, key, min);
-}
+    if (!root) return;
 
-template <typename E>
-void helper(BNode<E> *node, E key, vector<E> min)
-{
-    if (!node)
+    E result = -1;
+
+    while (root)
     {
-        if (min.empty()) return;
-
-        E dex = min[0];
-        for (size_t i = 1; i < min.size(); ++i)
-            if (dex < min[i])
-                dex = min[i];
-
-        cout << dex << endl;
+        if (root->element() < key)
+        {
+            result = root->element();
+            root = root->right();
+        }
+        else root = root->left();
     }
-    else if (node->key < key)
-    {
-        min.push_back(node->key);
-        helper(node->right, key, min);
-    }
-    else helper(node->left, key, min);
-}
 
-
-template <typename E>
-class BNode
-{
-    public:
-        E key;
-        BNode<E> *left;
-        BNode<E> *right;
-        BNode(E v, BNode<E> *l = NULL, BNode<E> *r = NULL) : key(v), left(l), right(r) {}
+    if (result != -1) cout << result;
 };
