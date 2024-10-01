0f6932485d70b2db59aaa253cc4afeccf7e5e0be
Khoa Tran <khoatrandd.139@gmail.com>
2024-10-01 10:23:17 +0700

    fixed parentheses

--- Special List/parentheses.cpp
+++ Special List/parentheses.cpp
@@ -1,25 +1,28 @@
 bool parentheses (const string & L)
 {
-    AStack<char> stack;
+    Stack<char> * stack = new AStack<char>;
     int len = L.length();
     
     for (int i = 0; i < len; i++)
     {
         char c = L[i];
         if (c == '(')
-            stack.push(c);
+            stack->push(c);
         else 
         {
-            if (!stack.length()) 
+            if (!stack->length()) 
                 return false;
             
-            char top = stack.topValue();
+            char top = stack->topValue();
             if (c == ')' && top != '(') 
                 return false;
                 
-            stack.pop();
+            stack->pop();
         }
     }
     
-    return !stack.length();
+    bool notEmpty = !stack->length();
+    delete stack;
+    
+    return notEmpty;
 }
